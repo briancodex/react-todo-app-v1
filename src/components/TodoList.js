cd10714db7c8e2ecdbc7989d0a5bd3e5e502c6f3
Adomas <sadasdasdas2312@gmail.com>
2022-10-23 13:25:21 +0300

    Update formatting and introduce issues

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import TodoForm from './TodoForm';
-import Todo from './Todo';
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
 
-  const addTodo = todo => {
+  const addTodo = (todo) => {
     if (!todo.text || /^\s*$/.test(todo.text)) {
       return;
     }
@@ -21,18 +21,20 @@ function TodoList() {
       return;
     }
 
-    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
   };
 
-  const removeTodo = id => {
-    const removedArr = [...todos].filter(todo => todo.id !== id);
+  const removeTodo = (id) => {
+    const removedArr = todos.filter((todo) => todo.id !== id);
 
     setTodos(removedArr);
   };
 
-  const completeTodo = id => {
-    let updatedTodos = todos.map(todo => {
-      if (todo.id === id) {
+  const completeTodo = (id) => {
+    var updatedTodos = todos.map((todo) => {
+      if (todo.id == id) {
         todo.isComplete = !todo.isComplete;
       }
       return todo;
